d55169d3cec7a670e710963bf5b38d362419407c
diksha <dikshawavale@gmail.com>
2026-08-02 22:38:06 +0530

    Completed Reports Module

--- reports/admin.py
+++ reports/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
+from .models import Report
 
-# Register your models here.
+@admin.register(Report)
+class ReportAdmin(admin.ModelAdmin):
+    list_display = (
+        'report_name',
+        'report_type',
+        'total_tokens',
+        'completed_tokens',
+        'cancelled_tokens',
+        'average_wait_time',
+        'generated_date',
+    )
+
+    search_fields = ('report_name',)
+    list_filter = ('report_type', 'generated_date')
--- reports/models.py
+++ reports/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+class Report(models.Model):
+    REPORT_TYPES = [
+        ('Daily', 'Daily'),
+        ('Weekly', 'Weekly'),
+        ('Monthly', 'Monthly'),
+    ]
+
+    report_name = models.CharField(max_length=100)
+    report_type = models.CharField(max_length=20, choices=REPORT_TYPES)
+    total_tokens = models.PositiveIntegerField(default=0)
+    completed_tokens = models.PositiveIntegerField(default=0)
+    cancelled_tokens = models.PositiveIntegerField(default=0)
+    average_wait_time = models.PositiveIntegerField(help_text="Minutes", default=0)
+    generated_date = models.DateField(auto_now_add=True)
+
+    def __str__(self):
+        return f"{self.report_name} ({self.report_type})"
